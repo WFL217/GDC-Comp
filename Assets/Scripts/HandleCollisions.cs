a00dacf9a2d035e3b7cb757fa49f20d8dfa3cc0e
WFL217 <jkaufman217@yahoo.com>
2017-05-22 23:58:16 -0700

    Add files via upload
    Prototype Assets

---
+++ Assets/Scripts/HandleCollisions.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleCollisions : MonoBehaviour
+{
+    //Scripts
+    private GameMananger gManager;
+    private GameObject playerPrefab;
+    private PlayerMovement player;
+
+    public float radius = 10.0f;
+
+    public float timer = 0.0f;
+    public float survivalTimer = 1.0f;
+
+
+
+
+    void Start()
+    {
+        #region Obtain the GameManager SCripts 
+        gManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameMananger>();
+        #endregion
+
+        #region Obtain the Player SCripts 
+        playerPrefab = GameObject.FindWithTag("Player");
+        player = playerPrefab.GetComponent<PlayerMovement>();
+        #endregion
+
+        #region Timer
+        if (transform.tag == "Player")
+        {
+            StartCoroutine(Timer());
+        }
+        #endregion
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        #region Player Collision Handler
+
+        if (this.transform.tag == "Player")
+        {
+            if (collider.gameObject.tag == "EnemyProjectile" || collider.gameObject.tag == "Enemy") //Set these to TAG [Your Tag's]
+            {
+                if (timer <= survivalTimer)
+                {
+                    Destroy(this.gameObject);
+                }
+                else
+                {
+                    timer = 0;
+
+                    #region Dimension Shift [Destroys Bullets in Radius around player]
+                    player.dimensionShift(true, player.isShifted, player.passTags);
+                    Collider[] colliders = Physics.OverlapSphere(collider.gameObject.transform.position, radius);
+                    foreach (Collider col in colliders)
+                    {
+                        if (col.tag == "Enemy01" || col.tag == "Enemy02" || col.tag == "EnemyProjectile")
+                        {
+                            Destroy(col.gameObject);
+                        }
+                    }
+
+                    #endregion
+                }
+            }
+        }
+        #endregion
+
+        #region Enemy Collision Handler
+        else
+        { 
+            if (gManager.enemyList.Contains(transform.tag))
+            {
+                if (collider.tag == "Projectile")
+                {
+                    transform.GetComponent<EnemyTemplateScript>().health--;
+                    Destroy(collider.gameObject);            
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    #region [Enumerators]
+
+    IEnumerator Timer()
+    {
+        timer++;
+        yield return new WaitForSeconds(1);
+        StartCoroutine(Timer());
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Transform player;
+
+    public Camera D1Camera;
+    public Camera D2Camera;
+
+    public Camera DEBUG;
+
+    public Vector3 D1PlayerPos;
+    public Vector3 D2PlayerPos;
+
+    private Vector3 DOffset = new Vector3(0,0,40);
+
+    public bool isInD1;
+    public bool Debug;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = this.gameObject.transform;
+
+ //       D1Camera = GameObject.FindGameObjectWithTag("D1").GetComponent<Camera>();
+ //       D2Camera = GameObject.FindGameObjectWithTag("D2").GetComponent<Camera>();
+
+//        DEBUG = GameObject.FindGameObjectWithTag("Debug").GetComponent<Camera>();
+
+
+
+        D1PlayerPos = player.position;
+        D2PlayerPos = player.position + DOffset;
+
+
+        isInD1 = true;
+        Debug = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TEMPCHOOSECAMERA();
+        KeyInputs();
+
+        if(isInD1 == true)
+        {
+            //Set Player Position
+            player.position = D1PlayerPos;
+
+            //Update Variables
+            D1PlayerPos = player.position;
+            D2PlayerPos = player.position + DOffset;
+        }
+
+        else
+        {
+            //Set Player Position
+            player.position = D2PlayerPos;
+
+            //Update Variables
+            D1PlayerPos = player.position - DOffset;
+            D2PlayerPos = player.position;
+        }
+
+	}
+
+
+    void TEMPCHOOSECAMERA()
+    {
+        if(isInD1 == true && Debug == false)
+        {
+            //Set Main Camera
+            D1Camera.gameObject.tag = "MainCamera";
+            D2Camera.gameObject.tag = "MainCamera";
+            DEBUG.gameObject.tag = "Debug";
+
+            D1Camera.gameObject.SetActive(true);
+            D2Camera.gameObject.SetActive(false);
+            DEBUG.gameObject.SetActive(false);
+        }
+
+        else if (isInD1 == false && Debug == false)
+        {
+            //Set Main Camera
+            D2Camera.gameObject.tag = "MainCamera";
+            D1Camera.gameObject.tag = "D1";
+            DEBUG.gameObject.tag = "Debug";
+
+            D1Camera.gameObject.SetActive(false);
+            D2Camera.gameObject.SetActive(true);
+            DEBUG.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            DEBUG.gameObject.tag = "MainCamera";
+            D1Camera.gameObject.tag = "D1";
+            D2Camera.gameObject.tag = "D2";
+
+            DEBUG.gameObject.SetActive(true);
+
+            D1Camera.gameObject.SetActive(false);
+            D2Camera.gameObject.SetActive(false);
+        }
+    }
+
+    void KeyInputs()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isInD1 = !isInD1;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug = !Debug;
+        }
+    }
+}
